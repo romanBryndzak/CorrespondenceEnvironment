791fb864505636373842d464293c18df6b6e758c
romanBryndzak <romabruzak@mail.com>
2021-12-25 18:04:51 +0200

    Create array users and message. Added function map, for create object user and message.

--- src/Pages/Messages/Messages.jsx
+++ src/Pages/Messages/Messages.jsx
@@ -1,6 +1,33 @@
 import s from "./Messages.module.css"
 import {NavLink} from "react-router-dom"
 
+const users = [
+    {id: "1", name: "Roman"},
+    {id: "2", name: "Maryna"},
+    {id: "3", name: "Rostyslav"},
+    {id: "4", name: "Snizhana"},
+    {id: "5", name: "Illia"}
+]
+
+const user = users.map(user => {
+    return (
+        <User name={user.name} id={user.id}/>
+    )
+})
+
+const messages = [
+    {id: 0, message: "Hello peopel!"},
+    {id: 1, message: "Hi freind!"},
+    {id: 2, message: "How are you?"},
+    {id: 3, message: "I am fine thanks. "},
+]
+
+const message = messages.map(message => {
+    return (
+        <Message message={message.message}/>
+    )
+})
+
 function User(props) {
     let path = "/messages/" + props.id
     return (
@@ -18,16 +45,10 @@ function Messages() {
     return (
         <div className={s.wrapper}>
             <div className={s.users}>
-                <User name="Roman" id="1"/>
-                <User name="Maryna" id="2"/>
-                <User name="Rostyslav" id="3"/>
-                <User name="Snizhana" id="4"/>
-                <User name="Illia" id="5"/>
+                {user}
             </div>
             <div className={s.dialogs}>
-                <Message message='Hello peopel!'/>
-                <Message message='Hi freind!'/>
-                <Message message='How are you?'/>
+                {message}
             </div>
         </div>
     )
